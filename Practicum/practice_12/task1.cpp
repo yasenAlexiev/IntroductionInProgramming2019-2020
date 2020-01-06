d091793f7ef52d47f978478b3e18fe0206b274ff
Yasen Alexiev <yassen4o222@abv.bg>
2020-01-06 22:51:42 +0200

    Commit tasks and examples for strings

---
+++ Practicum/practice_12/task1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+
+const char* names[] = { "Katerina", "Milanka", "Vediha", "Dimana", "Teodor",
+						"Stefan A.", "Luchezara", "Niya", "Toma", "Cveti", 
+						"Martin N.", "Silviya", "Dimitar", "Kalina", "Gabi", "Rado",
+						"Svetoslav", "Kiril", "Martin D." ,"Vladi", "Luben",
+						"Stefan H.", "Annamaria" };
+
+const size_t students_count = 23;
+
+
+const char* actions[] = { " prepodava na ", " smee se na ", " gazi ", " celuva :* ",
+						  " skacha vurhu ", " chupi krusta na ", " hodi na diskoteka s " };
+
+int main() {
+	// А) Изпишете срещу името на всеки колко е дължината му
+	// Пример:
+	// Katerina - 8
+	// Milanka - 7
+	// ....
+
+
+	// Б) Добавете нови действия в масива с действия
+	
+	// В) Изберете на рандом един студент, на рандом едно действие и още един студент (пак на рандом)
+	// Заделете точно толкова динамична памет, колкото е нужно, че да запишете
+	// двете имена и действието в нея. Изпишете полученото изречение на екрана.
+	
+	// Г) Въведете цяло положително число N от клавиатурата.
+	// Направете N изречения от горният вид.
+
+	return 0;
+}
