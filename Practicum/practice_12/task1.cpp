b25f859959a0cdf52d3d72ec51439356889e6d94
Yasen Alexiev <yassen4o222@abv.bg>
2020-01-10 02:18:51 +0200

    submit example and solution for two tasks

--- Practicum/practice_12/task1.cpp
+++ Practicum/practice_12/task1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 const char* names[] = { "Katerina", "Milanka", "Vediha", "Dimana", "Teodor",
 						"Stefan A.", "Luchezara", "Niya", "Toma", "Cveti", 
@@ -13,6 +15,26 @@ const size_t students_count = 23;
 const char* actions[] = { " prepodava na ", " smee se na ", " gazi ", " celuva :* ",
 						  " skacha vurhu ", " chupi krusta na ", " hodi na diskoteka s " };
 
+const size_t actions_count = 7;
+
+void generateAction() {
+	
+	const char* s1 = names[rand() % students_count];
+	const char* s2 = names[rand() % students_count];
+	const char* action = actions[rand() % actions_count];
+
+	char* str = new(std::nothrow) char[strlen(s1) + strlen(s2) + strlen(action) + 1];
+	strcpy(str, s1);
+	strcat(str, action);
+	strcat(str, s2);
+
+	std::cout << str << std::endl;
+
+	delete[] str;
+
+}
+
+
 int main() {
 	// А) Изпишете срещу името на всеки колко е дължината му
 	// Пример:
@@ -21,14 +43,27 @@ int main() {
 	// ....
 
 
+	for(int i = 0; i < students_count; ++i) {
+		std::cout << names[i] << " - " << strlen(names[i]) << std::endl;
+	}
+
 	// Б) Добавете нови действия в масива с действия
 	
-	// В) Изберете на рандом един студент, на рандом едно действие и още един студент (пак на рандом)
+	// В) Изберете на рандом един студент, едно действие и още един студент
 	// Заделете точно толкова динамична памет, колкото е нужно, че да запишете
 	// двете имена и действието в нея. Изпишете полученото изречение на екрана.
-	
+	srand(time(NULL));	
+	generateAction();
+
 	// Г) Въведете цяло положително число N от клавиатурата.
 	// Направете N изречения от горният вид.
+
+	size_t N;
+	std::cin >> N;
+
+	for(int i = 0; i < N; ++i) {
+		generateAction();	
+	}
 
 	return 0;
 }
